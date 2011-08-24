5457159fbfadd25ccd2c8ac117ca42993e42fdca
Casey West <casey@geeknest.com>
2011-08-24 14:57:10 -0400

    Re-instate proper gemset for your app.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
     # RVM bootstrap
     $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
     require "rvm/capistrano"
-    set :rvm_ruby_string, '1.9.2'
+    set :rvm_ruby_string, '1.9.2@example-geeknest-com'
     set :rvm_type, :user
 
     # bundler bootstrap
